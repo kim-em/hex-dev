e5e586c6917e523ebe15043b56348ae05309404c
Kim Morrison <kim@lean-fro.org>
2026-09-14 04:13:59 +0000

    fix: add symbolic test copyright header

--- HexBareissMathlib/SymbolicTests.lean
+++ HexBareissMathlib/SymbolicTests.lean
@@ -1,4 +1,8 @@
-
+/-
+Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
 import HexBareissMathlib.Tactic
 import Mathlib.Tactic.NormDet
 import Mathlib.Algebra.QuadraticAlgebra.Basic
