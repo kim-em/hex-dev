4f6c00f8f15794cd7d3cc6f83042c59a0db35ae5
Kim Morrison <kim@lean-fro.org>
2026-09-20 07:25:09 +0000

    test(poly-det): add required copyright header

--- HexPolyDet/PackedTests.lean
+++ HexPolyDet/PackedTests.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
 import HexPolyDet.Select
 
 open Hex.PolyDet Hex.Matrix Hex.MvPoly.Kernel Hex.Kronecker
